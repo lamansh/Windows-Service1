8b3f5940e001819f7c55c168039ce8043a88bd40
lamansh <30620287+lamansh@users.noreply.github.com>
2018-05-22 17:25:47 +0300

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -14,7 +14,7 @@ $pw = convertto-securestring -AsPlainText -Force -String "Certific@te0217"
 
 $credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist "kernel\sa.sharov",$pw
 
-$s = New-PSSession -ComputerName sharov
+$s = New-PSSession -ComputerName sharov 
 
 Invoke-Command -Session $s -Command {Stop-Service -Name "spooler"}
 
@@ -22,5 +22,6 @@ Invoke-Command -Session $s -Command {Remove-Item $Using:Dpath -Recurse}
 
 Copy-Item -ToSession $s -Path $spath -Destination $dpath -Recurse
 
-& $Runservice 
+#& $Runservice 
+Invoke-Command -Session $s -Command {Start-Service -Name "spooler"}
 
