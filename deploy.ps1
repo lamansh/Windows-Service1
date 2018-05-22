e373d42d277b8cac2e5be3b79bb99a70db154e64
lamansh <la_mansh@mail.ru>
2018-05-22 15:51:31 +0300

    deploy.ps1

---
+++ deploy.ps1
@@ -0,0 +1,26 @@
+param(
+	
+  [string]$Spath,
+  [string]$Dpath
+
+)
+
+$Command = "\src\WinService.Base\bin\Debug\WinService.Base.exe"
+
+$Runservice = "$Spath" + "$Command"
+
+
+$pw = convertto-securestring -AsPlainText -Force -String "Certific@te0217"
+
+$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist "sa.sharov",$pw
+
+$s = New-PSSession -ComputerName sharov -Credential $credentials
+
+Invoke-Command -Session $s -Command {Stop-Service -Name "spooler"}
+
+Invoke-Command -Session $s -Command {Remove-Item $Using:Dpath -Recurse}
+
+Copy-Item -ToSession $s -Path $spath -Destination $dpath -Recurse
+
+& $Runservice 
+
